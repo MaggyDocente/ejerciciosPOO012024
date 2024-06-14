408bab3c5fe7debd44214cf5239588424c240b4b
MaggyDocente <magali.sanchez@bue.edu.ar>
2024-06-14 12:25:46 -0300

    Camino de resolucion del ejercicio1 de la clase del 14/06

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Imprimir un menu.
+
+        //El cliente la opcion deseada
+
+        //Segun lo ingresado llama a una funcion especifica
 
     }
+
+    //crearContacto, necesito nombre y telefono
+    //Guardar un ArrayList el y otro el telefono
+
+    //buscarContacto, necesito su nombre
+    //Recorrer mi ArrayList de nombre para obtener la posicion, y buscar la misma posicion en el Array telefonos
+
+    //eliminarContacto. necesito su nombre
+    //Recorrer mi ArrayList de nombre para obtener la posicion, y buscar la misma posicion en el Array telefonos
 }
